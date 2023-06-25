aca87595a669d599a3e1d44d346bbc7ad08c267b
chandragumelar <hello.chandragumelar@gmail.com>
2023-06-25 23:57:17 +0700

    adding comment

--- nltk_word_freq.py
+++ nltk_word_freq.py
@@ -66,23 +66,30 @@ preProcessedTestSentence = nltk.tokenize.word_tokenize(testSentence.lower())
 # In[27]:
 
 
+# Create an empty DataFrame to store author probabilities
 testProbabilities = pd.DataFrame(columns=['author', 'jointProbability'])
 
+# Iterate over each author
 for author in wordFreqByAuthor.keys():
     jointProbability = 1.0
     
+    # Calculate the joint probability for each word in the pre-processed test sentence
     for word in preProcessedTestSentence:
         wordFreq = wordFreqByAuthor[author].freq(word)
         smoothedWordFreq = wordFreq + 0.000001
         jointProbability *= smoothedWordFreq
     
+    # Create a DataFrame row with the author and its joint probability
     output = pd.DataFrame([[author, jointProbability]], columns=['author', 'jointProbability'])
     testProbabilities = pd.concat([testProbabilities, output], ignore_index=True)
 
+# Find the author with the highest joint probability
 mostLikelyAuthor = testProbabilities.loc[testProbabilities['jointProbability'].idxmax(), 'author']
 
+# Print the joint probabilities for each author
 for _, row in testProbabilities.iterrows():
     print(f"{row['author']}: {row['jointProbability']}")
-
+    
+# Print the most likely author
 print(f"\nMost Likely Author: {mostLikelyAuthor}")
 

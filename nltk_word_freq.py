d20fc1c72791a486f45b27b0c78de329281f3e3a
chandragumelar <hello.chandragumelar@gmail.com>
2023-07-28 22:23:27 +0700

    initial commit

--- nltk_word_freq.py
+++ nltk_word_freq.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[1]:
 
 
 import os
@@ -10,10 +10,11 @@ import nltk
 from nltk.tokenize import word_tokenize
 
 
-# In[5]:
+# In[4]:
 
 
 # construct the path
+cwd = os.getcwd()
 file_path = os.path.join(cwd, 'data/train_author_nlp.csv')
 
 #read the file
@@ -21,75 +22,75 @@ df = pd.read_csv(file_path)
 df.head()
 
 
-# In[9]:
+# In[5]:
 
 
 # Group the df by author
-byAuthor = df.groupby('author')
+by_author = df.groupby('author')
 
 # Initialize ConditionalFreqDist to store word frequency by author
-wordFreqByAuthor = nltk.probability.ConditionalFreqDist()
+word_freq_by_author = nltk.probability.ConditionalFreqDist()
 
 
-# In[10]:
+# In[6]:
 
 
 # Iterate over each author group
-for name, group in byAuthor:
+for name, group in by_author:
     sentences = group['text'].str.cat(sep=' ')
     sentences = sentences.lower()
     tokens = word_tokenize(sentences)
     frequency = nltk.FreqDist(tokens)
-    wordFreqByAuthor[name] = frequency
+    word_freq_by_author[name] = frequency
 
 
-# In[16]:
+# In[7]:
 
 
 # Calculate and display the frequency of the word 'blood' for each author
 
 word = 'blood'
 
-for author in wordFreqByAuthor.keys():
+for author in word_freq_by_author.keys():
     print(f"Author: {author}")
-    print(f"Frequency of {word}: {wordFreqByAuthor[author].freq(word)}")
+    print(f"Frequency of {word}: {word_freq_by_author[author].freq(word)}")
     print()
 
 
-# In[21]:
+# In[8]:
 
 
-testSentence = "It was a dark and stormy night."
-preProcessedTestSentence = nltk.tokenize.word_tokenize(testSentence.lower())
+test_sentence = "It was a dark and stormy night."
+preprocessed_test_sentence = nltk.tokenize.word_tokenize(test_sentence.lower())
 
 
-# In[27]:
+# In[9]:
 
 
 # Create an empty DataFrame to store author probabilities
-testProbabilities = pd.DataFrame(columns=['author', 'jointProbability'])
+test_probabilities = pd.DataFrame(columns=['author', 'joint_probability'])
 
 # Iterate over each author
-for author in wordFreqByAuthor.keys():
-    jointProbability = 1.0
+for author in word_freq_by_author.keys():
+    joint_probability = 1.0
     
     # Calculate the joint probability for each word in the pre-processed test sentence
-    for word in preProcessedTestSentence:
-        wordFreq = wordFreqByAuthor[author].freq(word)
-        smoothedWordFreq = wordFreq + 0.000001
-        jointProbability *= smoothedWordFreq
+    for word in preprocessed_test_sentence:
+        word_freq = word_freq_by_author[author].freq(word)
+        smoothed_word_freq = word_freq + 0.000001
+        joint_probability *= smoothed_word_freq
     
     # Create a DataFrame row with the author and its joint probability
-    output = pd.DataFrame([[author, jointProbability]], columns=['author', 'jointProbability'])
-    testProbabilities = pd.concat([testProbabilities, output], ignore_index=True)
+    output = pd.DataFrame([[author, joint_probability]], columns=['author', 'joint_probability'])
+    test_probabilities = pd.concat([test_probabilities, output], ignore_index=True)
 
 # Find the author with the highest joint probability
-mostLikelyAuthor = testProbabilities.loc[testProbabilities['jointProbability'].idxmax(), 'author']
+most_likely_author = test_probabilities.loc[test_probabilities['joint_probability'].idxmax(), 'author']
 
 # Print the joint probabilities for each author
-for _, row in testProbabilities.iterrows():
-    print(f"{row['author']}: {row['jointProbability']}")
+for _, row in test_probabilities.iterrows():
+    print(f"{row['author']}: {row['joint_probability']}")
     
 # Print the most likely author
-print(f"\nMost Likely Author: {mostLikelyAuthor}")
+print(f"\nMost Likely Author: {most_likely_author}")
 
